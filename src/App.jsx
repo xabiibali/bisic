26a25c4a6a5c774a94b3b48f60395a5239226157
Xabiib Ali <119079937+xabiibali@users.noreply.github.com>
2023-06-12 17:54:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './features/counter/Counter';
+// import Counter from "./fetures/counter/Counter";
+function App() {
+  return (
+    <div className="App">
+       <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,42 @@
+import { useDispatch,  useSelector} from "react-redux"
+import { 
+      increment,
+      decrement,
+      reset,
+      incrementByAmount,
+    }  from "./counterSlice"
+ import { useState} from "react"
+function Counter() {
+    const count = useSelector((state) => state.moha.count)
+    const dispatch = useDispatch()
+    const [incrementAmount, setIncrementAmount] = useState(0)
+   const addValue = Number(incrementAmount) || 0;
+   const resetAll = () =>{
+    setIncrementAmount(0)
+    dispatch(reset())
+   }
+
+  return (
+    <div>
+       <p>{count}</p>
+       <diV>
+         <button onClick={() => dispatch(increment())}>+</button>
+         <button onClick={() => dispatch(decrement())}>-</button>
+        </diV> 
+        <div>
+            <input 
+            type="text"
+             value={incrementAmount}
+             onChange={(e) => setIncrementAmount(e.target.value)}
+            />
+        </div>
+        <div>
+         <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+         <button onClick={resetAll}> Reset</button>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Counter
